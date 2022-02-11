b54a1c57851501a3da2d9d5faf7435b993c39eab
Hyeonguk Ahn <ahu8867@gmail.com>
2022-02-11 22:21:19 +0900

    [chore]: global style 적용(reset.css)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './reset.css';
+import { Global } from '@emotion/react';
 import App from './App';
+import globalStyle from './globalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global styles={globalStyle} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
