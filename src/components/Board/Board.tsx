e1fa5471acba70f60f4f7dc4ac81da35cd252520
Hyeonguk Ahn <ahu8867@gmail.com>
2022-02-11 22:22:01 +0900

    [feature]: Board 컴포넌트 작성

---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,52 @@
+import { css } from '@emotion/react';
+import { BOARD_SIZE } from '../../utils/constants';
+
+interface Block {
+  color: string;
+  isAnswer: boolean;
+}
+interface BoardProps {
+  blocks: Block[];
+  onAnswerBlockClick: () => void;
+  onWrongBlockClick: () => void;
+}
+
+function Board({ blocks, onAnswerBlockClick, onWrongBlockClick }: BoardProps) {
+  return (
+    <div css={wrap}>
+      {blocks.map(({ color, isAnswer }, index) =>
+        isAnswer ? (
+          <div
+            key={index}
+            onClick={onAnswerBlockClick}
+            css={blockStyle(color, Math.sqrt(blocks.length))}
+          />
+        ) : (
+          <div
+            key={index}
+            onClick={onWrongBlockClick}
+            css={blockStyle(color, Math.sqrt(blocks.length))}
+          />
+        )
+      )}
+    </div>
+  );
+}
+
+const wrap = css`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  width: ${BOARD_SIZE}px;
+  height: ${BOARD_SIZE}px;
+`;
+
+const offset = 4;
+const blockStyle = (color: string, columns: number) => css`
+  background-color: ${color};
+  display: inline-block;
+  width: ${BOARD_SIZE / columns - offset}px;
+  height: ${BOARD_SIZE / columns - offset}px;
+`;
+
+export default Board;
